bab5de291f56a87e6b36b1f740065214f33bc8bc
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2017-05-12 14:50:27 +0900

    Testcase with three grid points

--- ex01/test/runtests.jl
+++ ex01/test/runtests.jl
@@ -30,4 +30,34 @@ const _lin_interp = my_lin_interp
             end
         end
     end
+
+    @testset "general case" begin
+        grid = 0:2
+        vals = [1, 3, 2.5]
+        itp = _lin_interp(grid, vals)
+
+        @testset "scalar input" begin
+            x, y = 1.2, 2.9
+            @test isapprox(itp(x), y)
+        end
+
+        @testset "end points" begin
+            for (x, y) in [(grid[1], vals[1]), (grid[end], vals[end])]
+                @test itp(x) == y
+            end
+        end
+
+        @testset "array inputs" begin
+            xys = [
+                ([0.5, 1.2], [2, 2.9]),  # Vector
+                (grid, vals),  # UnitRange
+                (0:2:2, vals[[1, 3]]),  # StepRange
+                (0:0.5:1.5, [1, 2, 3, 2.75]),  # FloatRange
+                (linspace(0, 1.5, 4), [1, 2, 3, 2.75])  # LinSpace
+            ]
+            for (x, y) in xys
+                @test isapprox(itp.(x), y)
+            end
+        end
+    end
 end
