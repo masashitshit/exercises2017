99d07c98c6e2f64a469b265c0cd56ea48bb70c58
Ryumei N <nswa17@gmail.com>
2017-06-27 21:32:20 +0900

    Fix runtests.jl (#41)
    * fix
    
    * Fix

--- ex02/test/runtests.jl
+++ ex02/test/runtests.jl
@@ -217,11 +217,11 @@ end
 
     @testset "many-to-one: Vector of Vectors" begin
         for d in matchings_many_to_one
-            s_prefs, c_prefs = mat2vecs.(d["s_prefs"], d["c_prefs"])
+            s_prefs, c_prefs = mat2vecs.([d["s_prefs"], d["c_prefs"]])
 
             # Default (Student proposal)
             s_matches, c_matches, indptr =
-                deferred_acceptance(s_prefs, c_prefs, d["caps"])
+                _deferred_acceptance(s_prefs, c_prefs, d["caps"])
             sort_matches!(c_matches, indptr)
             @test s_matches == d["s_matches_s_opt"]
             @test c_matches == d["c_matches_s_opt"]
@@ -252,7 +252,7 @@ end
 
                 # Default (Student proposal)
                 s_matches, c_matches, indptr =
-                    deferred_acceptance(s_prefs, c_prefs, d["caps"])
+                    _deferred_acceptance(s_prefs, c_prefs, d["caps"])
                 sort_matches!(c_matches, indptr)
                 @test s_matches == d["s_matches_s_opt"]
                 @test c_matches == d["c_matches_s_opt"]
